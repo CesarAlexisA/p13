3185195ed8298e2725f67c4f6aa8ec2e8a956f5f
52551 <52551@LAPTOP-CT8EMV28>
2023-05-25 17:15:22 -0600

    cesar alexis actualizacion de tranajp y lectura de archivos

---
+++ src/g4s21facae13/G4s21facae13.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21facae13;
+
+/**
+ *
+ * @author 52551
+ */
+public class G4s21facae13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      new JFPrincipal().setVisible(true);
+      
+    } 
+}
